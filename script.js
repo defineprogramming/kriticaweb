9dacd377d8ae7f2e177b80d2036ec53a0553dc29
Leo Cooper <113992401+defineprogramming@users.noreply.github.com>
2023-07-20 16:28:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,48 @@
-const countdown = () => {
-
-  const countDate = new Date('July 31, 2023 00:00:00').getTime();
-  const now = new Date().getTime();
-  const gap = countDate - now;
-
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const textDay = Math.floor(gap / day);
-  const textHour = Math.floor((gap % day) / hour);
-  const textMinute = Math.floor((gap % hour) / minute);
-  const textSecond = Math.floor((gap % minute) / second);
-
-  document.querySelector('.countdown #days').innerText = textDay;
-  document.querySelector('.countdown #hours').innerText = textHour;
-  document.querySelector('.countdown #minutes').innerText = textMinute;
-  document.querySelector('.countdown #seconds').innerText = textSecond;
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Kritica - Coming Soon</title>
   
-  document.querySelector('.countdown').style.opacity = 0;
-  setTimeout(() => {
-    document.querySelector('.countdown').style.opacity = 1;
-  }, 500);
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-};
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="styles.css">
+</head>
 
-setInterval(countdown, 1000);
+<body>
 
-  const animate = number => {
-    number.classList.add('animate');
-    setTimeout(() => {
-      number.classList.remove('animate');  
-    }, 500);
-  }
-
-  if(textDay < oldTextDay) {
-    animate(document.querySelector('.countdown #days'));
-  }
-
-  if(textHour < oldTextHour) {
-    animate(document.querySelector('.countdown #hours'));
-  }
-
-  if(textMinute < oldTextMinute) {
-    animate(document.querySelector('.countdown #minutes'));
-  }
+  <div class="container">
   
-  if(textSecond < oldTextSecond) { 
-    animate(document.querySelector('.countdown #seconds'));
-  }
+    <div class="logo">
+      <img src="kritica-logo.png" alt="Kritica">
+    </div>
+  
+    <div class="content">
+      <h1>The future of reading arrives in:</h1>
+      
+      <div class="countdown">
+        <div>
+          <span id="days">00</span>
+          <span>Days</span>
+        </div>  
+        <div>
+          <span id="hours">00</span>
+          <span>Hours</span> 
+        </div>
+        <div>
+          <span id="minutes">00</span>
+          <span>Mins</span>
+        </div>
+        <div>
+          <span id="seconds">00</span>
+          <span>Secs</span>
+        </div>
+      </div>
+    </div>
 
-  oldTextDay = textDay;
-  oldTextHour = textHour;
-  oldTextMinute = textMinute;
-  oldTextSecond = textSecond;
+  </div>
 
-};
+  <script src="script.js"></script>
+
+</body>
+</html>
